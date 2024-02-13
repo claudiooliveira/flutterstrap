6ceec69e387c6981a37857d02bc1832ca1d95b08
Claudio Oliveira <claudio_pg-sp@hotmail.com>
2024-02-12 21:07:59 -0300

    added default colors to color scheme, renamed abstract class

--- lib/src/theme/color_scheme.dart
+++ lib/src/theme/color_scheme.dart
@@ -1,1 +1,33 @@
-abstract class FlutterstrapDefaultColorScheme {}
+import 'package:flutter/material.dart';
+
+///
+/// Generate your own color scheme:
+/// http://mcg.mbitson.com/#!?mcgpalette0=%23f781a4#%2F
+///
+
+abstract class FlutterstrapColorScheme {
+  MaterialColor primary = const MaterialColor(0xffF781A4, {
+    50: Color(0xffFEF0F4),
+    100: Color(0xffFDD9E4),
+    200: Color(0xffFBC0D2),
+    300: Color(0xffF9A7BF),
+    400: Color(0xffF894B2),
+    500: Color(0xffF781A4),
+    600: Color(0xffF6799C),
+    700: Color(0xffF56E92),
+    800: Color(0xffF36489),
+    900: Color(0xffF15178),
+  });
+  MaterialColor secondary = const MaterialColor(0xff6551B4, {
+    50: Color(0xFFEDEAF6),
+    100: Color(0xFFD1CBE9),
+    200: Color(0xFFB2A8DA),
+    300: Color(0xFF9385CB),
+    400: Color(0xFF7C6BBF),
+    500: Color(0xff6551B4),
+    600: Color(0xFF5D4AAD),
+    700: Color(0xFF5340A4),
+    800: Color(0xFF49379C),
+    900: Color(0xFF37278C),
+  });
+}
--- lib/src/theme/theme_controller.dart
+++ lib/src/theme/theme_controller.dart
@@ -1,15 +1,14 @@
 import 'package:flutterstrap/flutterstrap.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterstrap/src/theme/color_scheme.dart';
 
 class FThemeController extends ChangeNotifier {
   FThemeController({
     required this.theme,
   });
 
-  FlutterstrapTheme<FlutterstrapDefaultColorScheme> theme;
+  FlutterstrapTheme<FlutterstrapColorScheme> theme;
 
-  setTheme(FlutterstrapTheme<FlutterstrapDefaultColorScheme> theme) {
+  setTheme(FlutterstrapTheme<FlutterstrapColorScheme> theme) {
     this.theme = theme;
     notifyListeners();
   }
